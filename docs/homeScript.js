6aae431c29e94e2dfbf778e69564df2075989786
Micah Beirne <micahb23@nycstudents.net>
2026-02-20 12:24:10 -0500

    Started js

---
+++ docs/homeScript.js
@@ -0,0 +1,18 @@
+function createTopNav () {
+    const links = [
+        {text: 'Home', href: ''}
+    ];
+    const nav = document.createElement('div');
+    nav.className = 'topnav';
+    links.forEach(l => {
+        const a = document.createElement('a');
+        a.href = l.href;
+        a.textContent = l.text;
+        if (l.floatRight) a.classList.add('right');
+        nav.appendChild(a);
+    });
+    parent.appendChild(nav);
+    return nav;
+}
+
+export default createTopNav;
