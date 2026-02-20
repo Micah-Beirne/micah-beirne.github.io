778b5af6ccb0a4f4ada6a8395bd09fe94d6c68f7
Micah-Beirne <micah.beirne.ny@gmail.com>
2026-02-20 15:42:22 -0500

    Implement hover effect for navigation links
    Added CSS styles for hover effect on navigation links.

--- docs/homeScript.js
+++ docs/homeScript.js
@@ -6,6 +6,16 @@ function createTopNav () {
         {text: 'Art', href: ''},
         {text: 'Contact', href: ''}
     ];
+    var css = '.navLink:hover {background-color: pink; box-shadow: 0px 0px 10px black}';
+    var style = document.createElement('style');
+    
+    if (style.styleSheet) {
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
+    
+    document.getElementsByTagName('head')[0].appendChild(style);
     const nav = document.createElement('div');
     nav.className = 'topnav';
     Object.assign(nav.style, {position: 'fixed', borderRadius: '25px', overflow: 'hidden', backgroundColor: 'lightpink', zIndex: '6'});
@@ -14,7 +24,7 @@ function createTopNav () {
         a.href = l.href;
         a.textContent = l.text;
         Object.assign(a.style, {float: 'left', display: 'block', color: 'white', textAlign: 'center', padding: '14px', textDecoration: 'none'});
-        Object.assign(a.style.hover, {backgroundColor: 'pink', boxShadow: '0px 0px 10px black'});
+        a.classList.add('.navLink');
         if (l.floatRight) a.classList.add('right');
         nav.appendChild(a);
     });
