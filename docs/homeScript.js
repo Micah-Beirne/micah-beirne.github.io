b512da6657ab4ef30abe3a11af532384b66a768c
Micah-Beirne <micah.beirne.ny@gmail.com>
2026-02-20 14:39:57 -0500

    Fix style property casing in homeScript.js

--- docs/homeScript.js
+++ docs/homeScript.js
@@ -9,17 +9,17 @@ function createTopNav () {
     const nav = document.createElement('div');
     const subnav = document.createElement('div');
     nav.className = 'topnav';
-    Object.assign(nav.style, {position: fixed, border-radius: 25px, overflow: hidden, background-color: lightpink, z-index: 6});
+    Object.assign(nav.style, {position: fixed, borderRadius: 25px, overflow: hidden, backgroundColor: lightpink, zIndex: 6});
     links.forEach(l => {
         const a = document.createElement('a');
         a.href = l.href;
         a.textContent = l.text;
-        Object.assign(a.style, {float: left, display: block, color: white, text-align: center, padding: 14px, text-decoration: none, :hover {background-color: pink; box-shadow: 0px 0px 10px black;}});
+        Object.assign(a.style, {float: left, display: block, color: white, textAlign: center, padding: 14px, textDecoration: none, :hover {backgroundColor: pink; boxShadow: 0px 0px 10px black;}});
         if (l.floatRight) a.classList.add('right');
         nav.appendChild(a);
     });
     const txt = document.createElement('p');
-    Object.assign(txt.style, {float: left, display: block, color: white, text-align: center, padding: 14px, text-decoration: none,});
+    Object.assign(txt.style, {float: left, display: block, color: white, textAlign: center, padding: 14px, textDecoration: none,});
     subnav.appendChild(txt);
     const referenceElement = document.getElementById('startLine');
     referenceElement.appendChild(nav);
