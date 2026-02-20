bfc28afec77ca106fc0e9abdf276d0b10d023a68
Micah-Beirne <micah.beirne.ny@gmail.com>
2026-02-20 14:42:35 -0500

    Fix style object syntax in homeScript.js

--- docs/homeScript.js
+++ docs/homeScript.js
@@ -9,17 +9,17 @@ function createTopNav () {
     const nav = document.createElement('div');
     const subnav = document.createElement('div');
     nav.className = 'topnav';
-    Object.assign(nav.style, {position: fixed, borderRadius: 25px, overflow: hidden, backgroundColor: lightpink, zIndex: 6});
+    Object.assign(nav.style, {position: 'fixed', borderRadius: '25px', overflow: 'hidden', backgroundColor: 'lightpink', zIndex: '6'});
     links.forEach(l => {
         const a = document.createElement('a');
         a.href = l.href;
         a.textContent = l.text;
-        Object.assign(a.style, {float: left, display: block, color: white, textAlign: center, padding: 14px, textDecoration: none, :hover {backgroundColor: pink; boxShadow: 0px 0px 10px black;}});
+        Object.assign(a.style, {float: 'left', display: 'block', color: 'white', textAlign: 'center', padding: '14px', textDecoration: 'none', :hover {backgroundColor: 'pink', boxShadow: '0px 0px 10px black';}});
         if (l.floatRight) a.classList.add('right');
         nav.appendChild(a);
     });
     const txt = document.createElement('p');
-    Object.assign(txt.style, {float: left, display: block, color: white, textAlign: center, padding: 14px, textDecoration: none,});
+    Object.assign(txt.style, {float: 'left', display: 'block', color: 'white', textAlign: 'center', padding: '14px', textDecoration: 'none',});
     subnav.appendChild(txt);
     const referenceElement = document.getElementById('startLine');
     referenceElement.appendChild(nav);
