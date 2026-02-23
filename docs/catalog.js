e9bf365798103b13f544d8482af7c48aa8298329
Micah-Beirne <micah.beirne.ny@gmail.com>
2026-02-22 20:16:45 -0500

    Create catalog.js for page generation
    Added a catalog of pages with images and descriptions.

---
+++ docs/catalog.js
@@ -0,0 +1,44 @@
+const pages = {
+    001: {
+        name: 'Stuff',
+        image: 'assets/stuff.png',
+        alt: 'An image containing things, perhaps stuff',
+        date: '01, 01, 0001',
+        description: 'An image of stuff',
+        tags: ['art'],
+        type: 'normal'
+    }
+    002: {
+        name: 'Things',
+        image: 'assets/things.png',
+        alt: 'An image containing things, perhaps stuff',
+        date: '01, 01, 0002',
+        description: 'An image of things',
+        tags: ['art', 'computer science'],
+        type: 'normal'
+    }
+};
+
+function generatePage () {
+    const id = window.location.hash.substring(1);
+    const container = document.createElement('div');
+    container.classList.add('container');
+  
+    const page = pages.id;
+    const title = page.name;
+    document.title = title;
+    const name = document.createElement('h1');
+    name.textContent = title;
+    containter.appendChild(name);
+    const image = document.createElement('img');
+    image.src = page.image;
+    image.alt = page.alt;
+    containter.appendChild(image);
+    const description = document.createElement('p');
+    description.textContent = page.description + '\n\n' + page.date;
+    containter.appendChild(description);
+  
+    const start = document.getElementById('startLine');
+    start.after(container);
+    return container;
+}
